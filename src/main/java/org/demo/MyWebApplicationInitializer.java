41c936071fe743f61dc2525b8650fce2e49292e7
zidom <zidom@126.com>
2014-03-26 13:48:12 +0800

    添加了常用的库

--- src/main/java/org/demo/MyWebApplicationInitializer.java
+++ src/main/java/org/demo/MyWebApplicationInitializer.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
@@ -16,11 +17,16 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
 	 */
 	@Override
 	public void onStartup(ServletContext container) {
-		
-		
-		
+
+		XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
+		applicationContext
+				.setConfigLocation("classpath:spring/application-config.xml");
+
+		DispatcherServlet dispatcher = new DispatcherServlet(applicationContext);
+		dispatcher.setContextConfigLocation("/WEB-INF/mvc-config.xml");
+
 		ServletRegistration.Dynamic registration = container.addServlet(
-				"dispatcher", new DispatcherServlet());
+				"dispatcher", dispatcher);
 		registration.setLoadOnStartup(1);
 		registration.addMapping("/*");
 	}
